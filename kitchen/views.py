0144808502284ce43c9c4ec25c9aae29772fc357
Roman Kolmakov <cerne313@gmail.com>
2022-11-10 17:04:42 +0200

    add basic structure for views-templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from kitchen.models import Cook, Dish
+
+
+def index(request):
+    cooks = Cook.objects.count()
+    dishes = Dish.objects.count()
+
+    context = {
+        "num_cooks": cooks,
+        "num_dishes": dishes,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishList(generic.ListView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class CookList(generic.ListView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
