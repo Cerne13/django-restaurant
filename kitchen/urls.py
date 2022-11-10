dd61e37463c810966024e06bd5f6d1870f574371
Roman Kolmakov <cerne313@gmail.com>
2022-11-10 17:58:47 +0200

    Add cooks, dish lists, add links to sidebar'

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, CookList, DishList
 
 urlpatterns = [
     path("", index, name="index"),
+    path("cooks/", CookList.as_view(), name="cooks-list"),
+    path("dishes/", DishList.as_view(), name="dishes-list")
 ]
 
 
